260742af9cf08c34c2d83d0de492e724b4a1f2e8
Pr4mpox <114173536+Brajamustii@users.noreply.github.com>
2024-08-30 22:38:32 +0700

    Update main.js

--- main.js
+++ main.js
@@ -7,18 +7,16 @@ const { DateTime } = require("luxon");
 
 class Fintopio {
   constructor() {
-    this.baseUrl = "https://fintopio-tg.fintopio.com/api";
+    this.baseUrl = process.env.BASE_URL || "https://fintopio-tg.fintopio.com/api";
     this.headers = {
       Accept: "application/json, text/plain, */*",
       "Accept-Encoding": "gzip, deflate, br",
       "Accept-Language": "en-US,en;q=0.9",
       Referer: "https://fintopio-tg.fintopio.com/",
-      "Sec-Ch-Ua":
-        '"Not/A)Brand";v="99", "Google Chrome";v="115", "Chromium";v="115"',
+      "Sec-Ch-Ua": '"Not/A)Brand";v="99", "Google Chrome";v="115", "Chromium";v="115"',
       "Sec-Ch-Ua-Mobile": "?1",
       "Sec-Ch-Ua-Platform": '"Android"',
-      "User-Agent":
-        "Mozilla/5.0 (Linux; Android 6.0; Nexus 5 Build/MRA58N) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/115.0.0.0 Mobile Safari/537.36",
+      "User-Agent": "Mozilla/5.0 (Linux; Android 6.0; Nexus 5 Build/MRA58N) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/115.0.0.0 Mobile Safari/537.36",
     };
   }
 
@@ -29,13 +27,12 @@ class Fintopio {
   async waitWithCountdown(seconds) {
     const spinners = ["|", "/", "-", "\\"];
     let i = 0;
+
     for (let s = seconds; s >= 0; s--) {
       readline.cursorTo(process.stdout, 0);
-      process.stdout.write(
-        `${spinners[i]} Waiting ${s} seconds to continue ${spinners[i]}`.cyan
-      );
+      process.stdout.write(`${spinners[i]} Menunggu ${s} detik untuk melanjutkan ${spinners[i]}`.cyan);
       i = (i + 1) % spinners.length;
-      await new Promise((resolve) => setTimeout(resolve, 1000));
+      await new Promise(resolve => setTimeout(resolve, 1000));
     }
     console.log("");
   }
@@ -48,7 +45,7 @@ class Fintopio {
       const response = await axios.get(`${url}?${userData}`, { headers });
       return response.data.token;
     } catch (error) {
-      this.log(`Authentication error: ${error.message}`, "red");
+      this.log(`Kesalahan autentikasi`, "red");
       return null;
     }
   }
@@ -65,7 +62,7 @@ class Fintopio {
       const response = await axios.get(url, { headers });
       return response.data;
     } catch (error) {
-      this.log(`Error fetching profile: ${error.message}`, "red");
+      this.log(`Kesalahan mengambil profil`, "red");
       return null;
     }
   }
@@ -80,9 +77,9 @@ class Fintopio {
 
     try {
       await axios.post(url, {}, { headers });
-      this.log("Daily check-in successful!", "green");
+      this.log("Check-in harian berhasil!", "green");
     } catch (error) {
-      this.log(`Daily check-in error: ${error.message}`, "red");
+      this.log(`Kesalahan check-in harian`, "red");
     }
   }
 
@@ -97,7 +94,7 @@ class Fintopio {
       const response = await axios.get(url, { headers });
       return response.data;
     } catch (error) {
-      this.log(`Error fetching farming state: ${error.message}`, "red");
+      this.log(`Kesalahan mengambil status farming: ${error.message}`, "red");
       return null;
     }
   }
@@ -112,19 +109,17 @@ class Fintopio {
 
     try {
       const response = await axios.post(url, {}, { headers });
-      const finishTimestamp = response.data.timings.finish;
+      const finishTimestamp = response.data.timings?.finish;
 
       if (finishTimestamp) {
-        const finishTime = DateTime.fromMillis(finishTimestamp).toLocaleString(
-          DateTime.DATETIME_FULL
-        );
-        this.log(`Starting farm...`, "yellow");
-        this.log(`Farming completion time: ${finishTime}`, "green");
+        const finishTime = DateTime.fromMillis(finishTimestamp).toLocaleString(DateTime.DATETIME_FULL);
+        this.log(`Memulai farming...`, "yellow");
+        this.log(`Waktu penyelesaian farming: ${finishTime}`, "green");
       } else {
-        this.log("No completion time available.", "yellow");
+        this.log("Tidak ada waktu penyelesaian yang tersedia.", "yellow");
       }
     } catch (error) {
-      this.log(`Error starting farming: ${error.message}`, "red");
+      this.log(`Kesalahan memulai farming: ${error.message}`, "red");
     }
   }
 
@@ -137,10 +132,12 @@ class Fintopio {
     };
 
     try {
-      await axios.post(url, {}, { headers });
-      this.log("Farm claimed successfully!", "green");
+      const response = await axios.post(url, {}, { headers });
+      if (response.status === 201) {
+        this.log("farming berhasil diklaim!", "green");
+      }
     } catch (error) {
-      this.log(`Error claiming farm: ${error.message}`, "red");
+      this.log(`Kesalahan mengklaim farming: ${error.message}`, "red");
     }
   }
 
@@ -149,46 +146,103 @@ class Fintopio {
       const userPart = userData.match(/user=([^&]*)/)[1];
       const decodedUserPart = decodeURIComponent(userPart);
       const userObj = JSON.parse(decodedUserPart);
-      return userObj.first_name || "Unknown";
+      return userObj.first_name || "Tidak Diketahui";
     } catch (error) {
-      this.log(`Error extracting first_name: ${error.message}`, "red");
-      return "Unknown";
+      this.log(`Kesalahan mengekstrak first_name: ${error.message}`, "red");
+      return "Tidak Diketahui";
     }
   }
 
-  calculateWaitTime(firstAccountFinishTime) {
-    if (!firstAccountFinishTime) return null;
+  async getTasks(token) {
+    const url = `${this.baseUrl}/hold/tasks`;
+    const headers = {
+      ...this.headers,
+      Authorization: `Bearer ${token}`,
+    };
 
-    const now = DateTime.now();
-    const finishTime = DateTime.fromMillis(firstAccountFinishTime);
-    const duration = finishTime.diff(now);
+    try {
+      const response = await axios.get(url, { headers });
+      const tasks = response.data.tasks || [];
+      for (const task of tasks) {
+        console.log(`ID Tugas: ${task.id}, Slug: ${task.slug}`);
+        await this.startTask(token, task.id);
+        await this.claimTask(token, task.id);
+      }
+      return tasks;
+    } catch (error) {
+      this.log(`Kesalahan mengambil tugas`, "red");
+      return [];
+    }
+  }
 
-    return duration.as("milliseconds");
+  async startTask(token, taskId) {
+    const url = `${this.baseUrl}/hold/tasks/${taskId}/start`;
+    const headers = {
+      ...this.headers,
+      Authorization: `Bearer ${token}`,
+      "Content-Type": "application/json",
+    };
+
+    try {
+      await axios.post(url, {}, { headers });
+      this.log(`Status Tugas ${taskId} berhasil dimulai!`, "green");
+      await this.waitWithCountdown(10);
+    } catch (error) {
+      this.log(`Kesalahan memulai tugas ${taskId}`, "red");
+    }
+  }
+
+  async claimTask(token, taskId) {
+    const url = `${this.baseUrl}/hold/tasks/${taskId}/claim`;
+    const headers = {
+      ...this.headers,
+      Authorization: `Bearer ${token}`,
+      "Content-Type": "application/json",
+    };
+
+    try {
+      const response = await axios.post(url, {}, { headers });
+      if (response.status === 201) {
+        this.log(`Klaim Tugas ${taskId} berhasil!`, "green");
+      } else if (response.status === 400) {
+        this.log(`Klaim Tugas ${taskId} gagal: Tugas tidak tersedia`, "red");
+      } else {
+        this.log(`Klaim Tugas ${taskId} gagal`, "red");
+      }
+    } catch (error) {
+      this.log(`Kesalahan mengklaim tugas ${taskId}`, "red");
+    }
   }
 
   async main() {
+    console.log(`
+  _____ _       _              _       
+ |  ___(_)_ __ | |_ ___  _ __ (_) ___  
+ | |_  | | '_ \\| __/ _ \\| '_ \\| |/ _ \\ 
+ |  _| | | | | | || (_) | |_) | | (_) |
+ |_|   |_|_| |_|\\__\\___/| .__/|_|\\___/ 
+                        |_|            
+                       `);
+    console.log(`Sumber     : https://github.com/Galkurta/Fintopio-BOT
+Update oleh: https://t.me/AirdropFamilyIDN`);
+    console.log(`Update Clear Task + Claim Task`);
     while (true) {
       const dataFile = path.join(__dirname, "data.txt");
       const data = await fs.readFile(dataFile, "utf8");
       const users = data.split("\n").filter(Boolean);
 
-      let firstAccountFinishTime = null;
-
       for (let i = 0; i < users.length; i++) {
         const userData = users[i];
-        const first_name = this.extractFirstName(userData);
-        console.log(
-          `${"=".repeat(5)} Account ${i + 1} | ${first_name.green} ${"=".repeat(
-            5
-          )}`.blue
-        );
+        const firstName = this.extractFirstName(userData);
+        console.log(`${"=".repeat(5)} Akun ${i + 1} | ${firstName.green} ${"=".repeat(5)}`.blue);
+
         const token = await this.auth(userData);
         if (token) {
-          this.log(`Login successful!`, "green");
+          this.log("Login berhasil!", "green");
           const profile = await this.getProfile(token);
           if (profile) {
             const balance = profile.balance;
-            this.log(`Balance: ${balance}`, "green");
+            this.log(`Saldo: ${balance}`, "green");
 
             await this.checkInDaily(token);
 
@@ -197,20 +251,11 @@ class Fintopio {
             if (farmingState) {
               if (farmingState.state === "idling") {
                 await this.startFarming(token);
-              } else if (
-                farmingState.state === "farmed" ||
-                farmingState.state === "farming"
-              ) {
-                const finishTimestamp = farmingState.timings.finish;
+              } else if (farmingState.state === "farmed" || farmingState.state === "farming") {
+                const finishTimestamp = farmingState.timings?.finish;
                 if (finishTimestamp) {
-                  const finishTime = DateTime.fromMillis(
-                    finishTimestamp
-                  ).toLocaleString(DateTime.DATETIME_FULL);
-                  this.log(`Farming completion time: ${finishTime}`, "green");
-
-                  if (i === 0) {
-                    firstAccountFinishTime = finishTimestamp;
-                  }
+                  const finishTime = DateTime.fromMillis(finishTimestamp).toLocaleString(DateTime.DATETIME_FULL);
+                  this.log(`Waktu penyelesaian farming: ${finishTime}`, "green");
 
                   const currentTime = DateTime.now().toMillis();
                   if (currentTime > finishTimestamp) {
@@ -220,24 +265,24 @@ class Fintopio {
                 }
               }
             }
+
+            const tasks = await this.getTasks(token);
+            if (tasks.length === 0) {
+              this.log("Tidak ada tugas yang tersedia untuk dikerjakan.", "yellow");
+            }
           }
         }
       }
-
-      const waitTime = this.calculateWaitTime(firstAccountFinishTime);
-      if (waitTime && waitTime > 0) {
-        await this.waitWithCountdown(Math.floor(waitTime / 1000));
-      } else {
-        this.log("No valid wait time, continuing loop immediately.", "yellow");
-        await this.waitWithCountdown(5);
-      }
+      // Tunggu selama 20 detik sebelum memulai iterasi berikutnya
+      this.log("Menunggu selama 20 detik sebelum iterasi berikutnya...", "cyan");
+      await this.waitWithCountdown(20);
     }
   }
 }
 
 if (require.main === module) {
   const fintopio = new Fintopio();
-  fintopio.main().catch((err) => {
+  fintopio.main().catch(err => {
     console.error(err);
     process.exit(1);
   });
